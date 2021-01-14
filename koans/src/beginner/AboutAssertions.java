ab537aeeb17ae5ce83d5560b55cd20f2e9326f1b
Vladislav Hutych <mandarineye@gmail.com>
2021-01-14 21:33:43 +0200

    feat(beginner): Solve: AboutKoans, AboutAssertions, AboutEquality, AboutStrings

--- koans/src/beginner/AboutAssertions.java
+++ koans/src/beginner/AboutAssertions.java
@@ -10,46 +10,55 @@ public class AboutAssertions {
     @Koan
     public void assertBooleanTrue() {
         // there are two possibilities, true or false, what would it be here?
-        assertTrue(__);
+        assertTrue(true);
     }
 
     @Koan
     public void assertBooleanFalse() {
-        assertFalse(__);
+        assertFalse(false);
     }
 
     @Koan
     public void assertNullObject() {
         // reference to the object can be null, a magic keyword, null, which means
         // that there is nothing there
-        assertNull(__);
+        assertNull(null);
     }
 
     @Koan
     public void assertNullObjectReference() {
-        Object someObject = __;
+        Object someObject = null;
         assertNull(someObject);
     }
 
     @Koan
     public void assertNotNullObject() {
         // but what when there should not be a null value?
-        assertNotNull(null);
+        Object someObject = new Object();
+        assertNotNull(someObject);
     }
 
     @Koan
     public void assertEqualsUsingExpression() {
-        assertTrue("Hello World!".equals(__));
+        final String HELLO_WORLD = "Hello World!";
+        assertTrue("Hello World!".equals(HELLO_WORLD));
     }
 
     @Koan
     public void assertEqualsWithAFewExpressions() {
-        assertEquals("Hello World!", __);
-        assertEquals(1, __);
-        assertEquals(2 + 2, __);
-        assertEquals(2 * 3, __);
-        assertEquals(3 - 8, __);
-        assertEquals(10 / 2, __);
+        final String HELLO_WORLD = "Hello World!";
+        final int ONE = 1;
+        final int TWO_PLUS_TWO = 4;
+        final int TWO_TIMES_THREE = 6;
+        final int THREE_MINUS_EIGHT = -5;
+        final int TEN_DIVIDED_BY_TWO = 5;
+
+        assertEquals("Hello World!", HELLO_WORLD);
+        assertEquals(1, ONE);
+        assertEquals(2 + 2, TWO_PLUS_TWO);
+        assertEquals(2 * 3, TWO_TIMES_THREE);
+        assertEquals(3 - 8, THREE_MINUS_EIGHT);
+        assertEquals(10 / 2, TEN_DIVIDED_BY_TWO);
     }
 
     @Koan
@@ -57,7 +66,8 @@ public class AboutAssertions {
         // Generally, when using an assertXXX methods, expectation is on the
         // left and it is best practice to use a String for the first arg
         // indication what has failed
-        assertEquals("The answer to 'life the universe and everything' should be 42", 42, __);
+        final int FORTY_TWO = 42;
+        assertEquals("The answer should be 42", 42, FORTY_TWO);
     }
 
     @Koan
@@ -69,7 +79,7 @@ public class AboutAssertions {
         assertEquals(original, same);
         assertEquals(original, different);
         // ...but only one refers to the same instance as the original.
-        assertSame(original, __);
+        assertSame(original, original);
     }
 
     @Koan
@@ -81,6 +91,6 @@ public class AboutAssertions {
         assertEquals(original, same);
         assertEquals(original, different);
         // ...but only one of them refers to a different instance.
-        assertNotSame(original, same);  // We want equal, but _not_ the same.
+        assertNotSame(original, different);  // We want equal, but _not_ the same.
     }
 }
